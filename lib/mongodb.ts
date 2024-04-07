1a5228fb9f39ddcb1b35464c60d4eb8295750d81
doudgaya <arbdoule@yahoo.com>
2024-04-07 10:08:46 +0100

    res

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 export const connectMongoDB = async () => {
     try {
         // @ts-ignore
-        await mongoose.connect(process.env.MONGODB_URI_VSCODE)
+        await mongoose.connect(process.env.DB)
         console.log('Connection Established')
     } catch (error) {
         console.log("Error Connecting to MongoDB", error)
