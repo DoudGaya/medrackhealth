cad3acb346a8ebdf3f30945c5010078293d2bffa
doudgaya <arbdoule@yahoo.com>
2024-04-08 15:36:55 +0100

    env

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,8 +1,7 @@
 import mongoose from "mongoose"
 export const connectMongoDB = async () => {
     try {
-        // @ts-ignore
-        await mongoose.connect(process.env.DB)
+        await mongoose.connect(process.env.DB || 'mongodb+srv://adaagad:QYHdfz7clx2boZse@cluster0.vsdn8go.mongodb.net/medrackhealth')
         console.log('Connection Established')
     } catch (error) {
         console.log("Error Connecting to MongoDB", error)
